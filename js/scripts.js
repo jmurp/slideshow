e7c647a571f7af95f91e557a6a14cf8fefc59948
Jake Murphy <jmurphy@protonmail.com>
2017-11-30 18:49:43 -0500

    modularized

--- js/scripts.js
+++ js/scripts.js
@@ -1,117 +1,154 @@
-console.log("scripts are running...");
-
-
-var imageFolder = "../images/";
-var imageSources = [];
-var currentImage = {
-    index: 0,
-    source: ""
-};
-var thumbnails = document.getElementById("thumbnails");
-var imageInView = document.getElementById("imageInView");
-var rightArrow = document.getElementById("right");
-var leftArrow = document.getElementById("left");
-var playPause = document.getElementById("playPause");
-var playingID;
-var slideInterval = 30000;//30s
-var intervalInput = document.getElementById("intervalInput");
-var intervalSubmit = document.getElementById("intervalSubmit");
-
-$.ajax({
-    url : imageFolder,
-    success: function (data) {
-        $(data).find("a").attr("href",function (i,val) {
-            if (val.match(/\.(jpe?g|png)$/)) {
-                console.log("ajax request found file: "+val);
-                if (!val.includes("images/")) {
-                    val = "images/"+val;
-                    console.log("setting val to : "+val);
-                }
-                imageSources.push(val);
+(function(){
+    
+    var slideshow = {
+        
+        images: [],
+        currentImageIndex: 0,
+        currentInterval: 30,
+        intervalID: 0,
+        playing: false,
+        next: function(){
+            this.currentImageIndex++;
+            if (this.currentImageIndex > this.images.length-1) {
+                this.currentImageIndex = 0;
             }
-        });
-    }
-}).done(loadSite);
+            this.render();
+        },
+        previous: function(){
+            this.currentImageIndex--;
+            if (this.currentImageIndex < 0) {
+                this.currentImageIndex = this.images.length-1;
+            }
+            this.render();
+        },
+        playPause: function() {
+            if (this.playing) {
+                this.stopSlideshow();
+                this.playPauseButton.classList.remove("playing");
+            }else {
+                this.startSlideshow();
+                this.playPauseButton.classList.add("playing");
+            }
+        },
+        clickThumb: function(){
+            slideshow.currentImageIndex = this.id.split("_")[1];
+            slideshow.render();
+        },
+        startSlideshow: function(){
+            this.intervalID = setInterval(function(){
+                slideshow.next();
+            },this.currentInterval * 1000);
+            this.playing = true;
+        },
+        stopSlideshow: function(){
+            clearInterval(this.intervalID);
+            this.playing = false;
+        },
+        setIntervalValue: function(){
+            
+            let userInput = parseInt(this.intervalInput.value);
+            if (isNaN(userInput)) {
+                return;
+            }else {
+                if (userInput < 1) { userInput = 1; }
+                this.currentInterval = userInput;
+                if (this.playing) {
+                    this.stopSlideshow();
+                    this.startSlideshow();   
+                }
+            }
+            
+        },
+        loadImages: function(callback){
+            
+            $.ajax({
+                url: "../images/",
+                success: function(data) {
+                    $(data).find("a").attr("href",function(i,val){
+                        if (val.match(/\.(jpe?g|png)$/)){
+                            //console.log("found image: "+val);
+                            if (!val.includes("images/")) {
+                                val = "../images/"+val;
+                                //console.log("setting to: "+val);
+                            }else if (val.substr(0,2) != "..") {
+                                val = ".."+val;
+                                //console.log("setting to: "+val);
+                            }
+                            slideshow.images.push(val);
+                        }
+                    });
+                },
+                complete: function(){
+                    callback.call(slideshow);
+                }
+            });
+            
+        },
+        cacheDOM: function(){
+            
+            this.thumbnails = document.getElementById("thumbnails");
+            this.imageView = document.getElementById("imageView");
+            this.leftButton = document.getElementById("left");
+            this.rightButton = document.getElementById("right");
+            this.playPauseButton = document.getElementById("playPause");
+            this.intervalSubmit = document.getElementById("intervalSubmit");
+            this.intervalInput = document.getElementById("intervalInput");
+            
+        },
+        render: function(){
+            
+            if (this.thumbnails.children.length == 0) {
+                for (let i = 0; i < this.images.length; i++) {
+                    let thumb = document.createElement("div");
+                    thumb.classList.add("thumbnail");
+                    thumb.id = "thumb_"+i;
+                    thumb.style.backgroundImage = "url('"+this.images[i]+"')";
+                    this.thumbnails.appendChild(thumb);
+                }
+            }
+            
+            let imageSource = this.images[this.currentImageIndex];
+            this.imageView.style.backgroundImage = "url('"+imageSource+"')";
+            
+            
+        },
+        bindEvents: function(){
 
-function loadSite() {
-    if (imageSources.length == 0) {
-        console.log("...no images...");
-        return;
-    }
-    for (i = 0; i < imageSources.length; i++) {
-        addThumb(imageSources[i]);
-    }
-    setImageInView(imageSources[0]);
-    rightArrow.addEventListener("click",rightArrowClick);
-    leftArrow.addEventListener("click",leftArrowClick);
-    document.addEventListener("keydown",function(e) {
-        if (e.keyCode == 37) {
-            leftArrowClick();
-        }else if (e.keyCode == 39) {
-            rightArrowClick();
-        }else if (e.keyCode == 32) {
-            playPauseClick();
+            this.leftButton.addEventListener("click",this.previous.bind(slideshow));
+            this.rightButton.addEventListener("click",this.next.bind(slideshow));
+            this.playPauseButton.addEventListener("click",this.playPause.bind(slideshow));
+            document.addEventListener("keydown",function(e) {
+               if (e.keyCode == 37) {
+                   slideshow.previous();
+               }else if (e.keyCode == 39) {
+                   slideshow.next();
+               }else if (e.keyCode == 32) {
+                   slideshow.playPause();
+               }
+            });
+            let next = this.thumbnails.firstChild;
+            while (next != null) {
+                next.addEventListener("click",this.clickThumb.bind(next));
+                next = next.nextSibling;
+            }
+            this.intervalSubmit.addEventListener("click",this.setIntervalValue.bind(slideshow));
+            
+        },
+        init: function(){
+            
+            let finish = function(){
+                this.cacheDOM();
+                this.render();
+                this.bindEvents();
+                this.startSlideshow();
+            };
+            this.loadImages(finish);
+            
         }
-    });
-    playPause.addEventListener("click",playPauseClick);
-    playPauseClick();
-    intervalSubmit.addEventListener("click",setIntervalValue);
-}
-function addThumb(imageSource) {
-    var thumb = document.createElement("div");
-    thumb.classList.add("thumbnail");
-    thumb.style.backgroundImage = "url('"+imageSource+"')";
-    console.log("adding thumb with backgroundImage: "+imageSource);
-    thumb.addEventListener("click",thumbClick.bind(thumb,imageSource));
-    thumbnails.appendChild(thumb);
-}
-function setImageInView(imageSource) {
-    var indexOfThisImage = imageSources.indexOf(imageSource);
-    currentImage.index = indexOfThisImage;
-    currentImage.source = imageSource;
-    updateImageView();
-}
-function updateImageView() {
-    console.log("setting img tag src to: "+currentImage.source);
-    imageInView.setAttribute("src",currentImage.source);
-}
-function thumbClick(imageSource) {
-    setImageInView(imageSource);
-}
-function rightArrowClick() {
-    var nextIndex = currentImage.index + 1;
-    if (nextIndex >= imageSources.length) { nextIndex = 0; }
-    currentImage.index = nextIndex;
-    currentImage.source = imageSources[nextIndex];
-    updateImageView();
-}
-function leftArrowClick() {
-    var nextIndex = currentImage.index - 1;
-    if (nextIndex <= 0) { nextIndex = imageSources.length - 1; }
-    currentImage.index = nextIndex;
-    currentImage.source = imageSources[nextIndex];
-    updateImageView();
-}
-function playPauseClick() {
-    if (playPause.classList.contains("playing")) {
-        playPause.classList.remove("playing");
-        clearInterval(playingID);
-    }else {
-        playPause.classList.add("playing");
-        playingID = setInterval(rightArrowClick,slideInterval);
-    }
-}
-function setIntervalValue() {
-    var newInterval = parseInt(intervalInput.value);
-    if (isNaN(newInterval)) {
-        return;
-    }else {
-        if (newInterval < 1) { newInterval = 1; }
-        slideInterval = newInterval * 1000;
-        clearInterval(playingID);
-        playingID = setInterval(rightArrowClick,slideInterval);
-    }
-}
-
-
-console.log("scripts are done running");
+        
+        
+    };
+    
+    slideshow.init();
+    
+})();
